7e291f41b6ec60880b5d70782ff4cde137632282
KC Barrett <kbarre123@gmail.com>
2014-07-19 17:55:59 -0500

    Added condition ro only show valid reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,16 @@
 class ReviewsController < ApplicationController
     skip_authorization_check
     skip_before_action :authenticate_user!
-  
+    
     def create
-    @business = Business.find(params[:business_id])
-    @review = @business.reviews.create(review_params)
-    flash[:notice] = 'Review posted!'
-    redirect_to business_path(@business)
+        @business = Business.find(params[:business_id])
+        @review = @business.reviews.create(review_params)
+        if @review.save
+            redirect_to business_path(@business)
+            flash[:notice] = 'Review posted!'
+        else
+            redirect_to business_path(@business)
+        end
     end
 
     private
