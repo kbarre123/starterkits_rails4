1ed203bb0fbaf9c045d6ca13aab0a476314a0ab9
KC Barrett <kbarre123@gmail.com>
2014-08-06 14:26:23 -0500

    Added user's comments to their home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   load_and_authorize_resource
 
     def show
+        @user = params[:id].blank? ? current_user : User.find(params[:id])
     end
     
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,38 +7,34 @@ class Ability
   include CanCan::Ability
 
   def initialize(user = User.new)
-    if user.nil?
-      can :read, :all
-    else
-      can :manage, User, id: user.id
-      can :manage, Authentication, user_id: user.id
-      can :read, :all
-      can :create, Review
-      can [:update, :destroy], Review, :user_id => user.id
+    can :manage, User, id: user.id
+    can :manage, Authentication, user_id: user.id
+    can :read, :all
+    can :create, Review
+    can :manage, Review, :user_id => user.id
 
-      if user.is_admin? && defined? RailsAdmin
-        # Allow everything
-        can :manage, :all
+    if user.is_admin? && defined? RailsAdmin
+      # Allow everything
+      can :manage, :all
 
-        # RailsAdmin
-        # https://github.com/sferik/rails_admin/wiki/CanCan
-        # can :access, :rails_admin
-        # can :dashboard
+      # RailsAdmin
+      # https://github.com/sferik/rails_admin/wiki/CanCan
+      # can :access, :rails_admin
+      # can :dashboard
 
-        # RailsAdmin checks for `collection` scoped actions:
-        # can :index, Model             # included in :read
-        # can :new, Model               # included in :create
-        # can :export, Model
-        # can :history, Model           # for HistoryIndex
-        # can :destroy, Model           # for BulkDelete
+      # RailsAdmin checks for `collection` scoped actions:
+      # can :index, Model             # included in :read
+      # can :new, Model               # included in :create
+      # can :export, Model
+      # can :history, Model           # for HistoryIndex
+      # can :destroy, Model           # for BulkDelete
 
-        # RailsAdmin checks for `member` scoped actions:
-        # can :show, Model, object      # included in :read
-        # can :edit, Model, object      # included in :update
-        # can :destroy, Model, object   # for Delete
-        # can :history, Model, object   # for HistoryShow
-        # can :show_in_app, Model, object
-      end
+      # RailsAdmin checks for `member` scoped actions:
+      # can :show, Model, object      # included in :read
+      # can :edit, Model, object      # included in :update
+      # can :destroy, Model, object   # for Delete
+      # can :history, Model, object   # for HistoryShow
+      # can :show_in_app, Model, object
     end
   end
 end
