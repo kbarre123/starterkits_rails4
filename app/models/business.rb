15470155133fe295d248d1dafaf15b57bf446732
KC Barrett <kbarre123@gmail.com>
2014-08-06 11:52:29 -0500

    Added extra field-validations to _form

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,5 +1,12 @@
 class Business < ActiveRecord::Base
     has_many :reviews, dependent: :destroy
     validates :title, presence: true, length: { minimum: 5 }
+    validates :category, presence: true
+    validates :street_no, presence: true
+    validates :street, presence: true
+    validates :city, presence: true
+    validates :zip_code, presence: true
+    validates :country, presence: true
+    validates :telephone, presence: true
     validates_associated :reviews
 end
