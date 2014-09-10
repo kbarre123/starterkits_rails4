cf7400101837f68eec32716a669444f0015146d6
KC Barrett <kbarre123@gmail.com>
2014-09-10 11:59:49 -0500

    Paginate reviews & comments on their respective associated models' show pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @comments = @post.comments.paginate(page: params[:page], per_page: 5)
   end
 
   def edit
