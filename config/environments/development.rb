7d7232b9f0452dae1418cb0a4f61ab39f4664b00
KC Barrett <kbarre123@gmail.com>
2014-09-10 23:22:10 -0500

    Test gmap4rails on Heroku; temp fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,8 @@ Rails.application.configure do
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
-  config.assets.raise_runtime_errors = false
+  config.assets.raise_runtime_errors = true
+  config.assets.precompile += %w( head.js ie.js emails/inline.css emails/include.css )
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
