2c7d269b5842984c9797ec8cd596148762d434c3
KC Barrett <kbarre123@gmail.com>
2014-10-06 11:23:44 -0500

    Stage Facebook 'like' button & scripts

--- app/assets/javascripts/businesses.js
+++ app/assets/javascripts/businesses.js
@@ -13,3 +13,21 @@ $(function() {
     return false;
   });
 });
+
+/*onLoad(function() {
+    window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '467842340019517',
+          xfbml      : true,
+          version    : 'v2.1'
+        });
+    };
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk')
+    );    
+});*/
