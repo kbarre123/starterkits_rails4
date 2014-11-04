f6b3e6da5369408ca5d364c88a065fd4070f7c74
KC Barrett <kbarre123@gmail.com>
2014-11-04 16:53:45 -0600

    Add connection params for elasticsearch to model as troubleshoot

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,5 @@
 class Business < ActiveRecord::Base
+    Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL'], log: true
     include Elasticsearch::Model
     include Elasticsearch::Model::Callbacks
 
